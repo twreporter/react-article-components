196e2f9bfe35172fb9663c5150e8eacc86eeb80f
yucj <nakts0123@gmail.com>
2019-04-12 14:03:40 +0800

    Update article body

--- src/components/article-page.js
+++ src/components/article-page.js
@@ -1,16 +1,24 @@
 import React, { PureComponent } from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import Body from './body'
 import LeadingBlock from './leading-block'
+import get from 'lodash/get'
+
+const _ = {
+  get,
+}
 
 export default class Article extends PureComponent {
-  static propTypes = {}
+  static propTypes = {
+    post: PropTypes.object.isRequired,
+  }
 
   render() {
+    const { post } = this.props
     return (
       <div>
         <LeadingBlock />
-        <Body />
+        <Body content={_.get(post, 'content.api_data')} />
       </div>
     )
   }
