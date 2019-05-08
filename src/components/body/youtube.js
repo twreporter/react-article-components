f6b120a1a4871a1e72f85cce60e8f4e282f57250
nickhsine <nickhsine97753017@gmail.com>
2019-05-08 10:57:31 +0800

    Extract co-used styled components. Add `components/body/styled.js`

--- src/components/body/youtube.js
+++ src/components/body/youtube.js
@@ -1,32 +1,15 @@
 import React from 'react'
-import mq from '../../utils/media-query'
 import predefinedPropTypes from '../../constants/prop-types'
+import predefinedStyled from './styled'
 import styled from 'styled-components'
-import typography from '../../constants/typography'
 import get from 'lodash/get'
 
 const _ = {
   get,
 }
 
-const mockup = {
-  captionWidth: {
-    desktop: 180,
-    hd: 265,
-  },
-}
-
-const youtube = {
-  width: 734,
-  height: 417,
-}
-
-const Block = styled.div`
-  width: 100%;
-  padding-bottom: ${(youtube.height / youtube.width) * 100}%;
-  position: relative;
-  float: right;
-  margin-bottom: 60px;
+const Block = styled(predefinedStyled.Multimedia.Block)`
+  padding-bottom: 56.25%;
 `
 
 const Iframe = styled.iframe`
@@ -37,26 +20,7 @@ const Iframe = styled.iframe`
   height: 100%;
 `
 
-const Desc = styled.p`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  transform: translateY(100%);
-  border-bottom: 2px solid ${props => props.theme.colors.primary.line};
-  color: ${props => props.theme.colors.base.text};
-  line-height: 1.36;
-  letter-spacing: 0.5px;
-  font-weight: ${typography.font.weight.light};
-  font-size: 14px;
-  padding: 15px 0;
-  margin: 0;
-  ${mq.desktopOnly`
-    width: ${mockup.captionWidth.desktop}px;
-  `}
-  ${mq.hdOnly`
-    width: ${mockup.captionWidth.hd}px;
-  `}
-`
+const Desc = predefinedStyled.Multimedia.Caption
 
 export default class Youtube extends React.PureComponent {
   static propTypes = {
