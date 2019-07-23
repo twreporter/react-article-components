8e3482d612f7f66fc26483e4511f378ac287cfe4
yucj <nakts0123@gmail.com>
2019-07-23 16:09:56 +0800

    Fix typo

--- src/components/body/audio/time.js
+++ src/components/body/audio/time.js
@@ -5,7 +5,7 @@ function getMinSecStr(secs) {
   const minutes = Math.floor(secs / 60)
   const seconds = Math.floor(secs - minutes * 60)
   return `${minutes}:${
-    seconds < 10 ? 0 + seconds.toString() : seconds.toString()
+    seconds < 10 ? `0${seconds.toString()}` : seconds.toString()
   }`
 }
 
